a7db695b52f4b781fae3d40d650631148234b187
Manuel Cortez <Manuel Cortez>
2017-04-03 17:50:18 -0400

    Added in the blog practice from the book
    Added in the folder containing al the files and information from the
    blog project that was from the Real Python book.

---
+++ bloggy_project/blog/models.py
@@ -0,0 +1,15 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+class Post(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    tag = models.CharField(max_length = 20, blank=True, null=True)
+    image = models.ImageField(upload_to="images", blank=True, null=True)
+    views = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
